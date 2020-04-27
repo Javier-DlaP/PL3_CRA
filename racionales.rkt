c45e16e1897b2b0360734a7501e2fef963dbb9f1
Enrique Coronado <enrique.developer@outlook.com>
2020-04-27 21:41:43 +0200

    mayor_racional

--- racionales.rkt
+++ racionales.rkt
@@ -7,6 +7,15 @@
                           (esceroent(primero x))))
 ;(escero_racional ((par cero) uno))
 
+
+
+(define mayor_racional(lambda (r)
+                        (lambda (s)
+                        ((esmayorent
+                          ((prodent (primero r)) ((cocienteent ((mcment (segundo r)) (segundo s))) (segundo r))))
+                         ((prodent (primero s)) ((cocienteent ((mcment (segundo r)) (segundo s))) (segundo s)))))))
+;((mayor_racional ((par tres) cuatro)) ((par tres) cinco))
+
 ;Javi
 
 
