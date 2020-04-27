d02654b2a37cab77a663145eeab8618e8858bab6
Javi <javirachi99@hotmail.es>
2020-04-27 20:49:49 +0200

    esigual_racional

--- racionales.rkt
+++ racionales.rkt
@@ -8,3 +8,9 @@
 ;Javi
 
 
+(define si (lambda (p x y) (p x y)))
+
+(define esigual_racional (lambda (x y)
+                           ((esigualent
+                             ((prodent (primero x)) (segundo y)))
+                             ((prodent (segundo x)) (primero y)))))
